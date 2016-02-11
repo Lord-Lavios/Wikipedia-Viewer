d004f5d8920f7bce8209332c6124caa12f0fc3bb
Kush Jain <Lord-Lavios@users.noreply.github.com>
2016-02-12 00:05:44 +0530

    Updated for undefined error, it showed undefined results

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var basicUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+var basicUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='; //jsonp
 var search = $('input').val();
 var searchBefore = '';
 var limit = $('#how :selected').text();
@@ -20,12 +20,16 @@ $(document).ready(function() {
 				success: function (data) {
 					$('ul').children().remove();
 					for(var i =0;i<limit;i++) {
-						$('ul').append(
-							'<li>' 
-							+ '<a href=' + data[3][i] + ' target="_blank">'
-							+ '<h3>' + data[1][i] + '</h3>'
-							+ '<p>' + data[2][i] + '</p>'
-							+ '</a>' + '</li>').hide().fadeIn(200);
+						if(data[1][i] !== undefined && data[2][i] !== undefined && data[3][i] !== undefined) {
+							$('ul').append(
+								'<li>' 
+								+ '<a href=' + data[3][i] + ' target="_blank">'
+								+ '<h3>' + data[1][i] + '</h3>'
+								+ '<p>' + data[2][i] + '</p>'
+								+ '</a>' + '</li>').hide().fadeIn(200);
+						} else {
+							i += 1;
+						}
 					}
 				}
 			});
