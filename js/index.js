7e4c93c3111bfd7dfea7c37c619e7af6ce3e6b36
Kush Jain <kushj2008@gmail.com>
2016-05-30 17:33:08 +0530

    Improved header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var basicUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=';
+var basicUrl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='; //jsonp
 var search = $('input').val();
 var searchBefore = '';
 var limit = $('#how :selected').text();
@@ -6,7 +6,7 @@ var beforeLimit = 10;
 var url;
 
 $(document).ready(function() {
-	setInterval(renew, 500);
+	setInterval(renew, 100);
 	function renew() {
 		search = $('input').val();
 		limit = $('#how :selected').text();
@@ -20,12 +20,16 @@ $(document).ready(function() {
 				success: function (data) {
 					$('ul').children().remove();
 					for(var i =0;i<limit;i++) {
-						$('ul').append(
-							'<li>' 
-							+ '<a href=' + data[3][i] + ' target="_blank">'
-							+ '<h3>' + data[1][i] + '</h3>'
-							+ '<p>' + data[2][i] + '</p>'
-							+ '</a>' + '</li>').hide().fadeIn(900);
+						if(data[1][i] !== undefined && data[2][i] !== undefined && data[3][i] !== undefined) {
+							$('ul').append(
+								'<li>' 
+								+ '<a href=' + data[3][i] + ' target="_blank">'
+								+ '<h3>' + data[1][i] + '</h3>'
+								+ '<p>' + data[2][i] + '</p>'
+								+ '</a>' + '</li>').hide().fadeIn(200);
+						} else {
+							i += 1;
+						}
 					}
 				}
 			});
